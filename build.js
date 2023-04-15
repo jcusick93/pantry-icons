1cb1959dedb880c0e99f3447dd566c6c4dc37ab5
Josh Cusick <josh.m.cusick@gmail.com>
2023-04-14 23:44:03 -0400

    Updates

--- build.js
+++ build.js
@@ -3,11 +3,12 @@ const fs = require('fs')
 const path = require('path')
 require('dotenv').config();
 
+// the api key stored in .env
 const figmaApiKey = process.env.FIGMA_API_KEY;
+// the doc id stored in .env
 const figmaDocId = process.env.FIGMA_DOC_ID;
 
 
-const docId = figmaDocId
 
 
 const options = {
@@ -16,23 +17,22 @@ const options = {
   }
 }
 
-axios(`https://api.figma.com/v1/files/${docId}`, options).then(res => {
+axios(`https://api.figma.com/v1/files/${figmaDocId}`, options).then(res => {
   const file = res.data
   const ids = Object.keys(file.components).toString()
 
   ;(async () => {
-    const res = await axios(`https://api.figma.com/v1/images/${docId}/?ids=${ids}&format=svg`, options)
+    const res = await axios(`https://api.figma.com/v1/images/${figmaDocId}/?ids=${ids}&format=svg`, options)
     const urls = res.data.images
 
       for (const [componentId, componentUrl] of Object.entries(urls)) {
         const componentNode = file.components[componentId];
         const componentName = componentNode.name;
-        const name = componentName.replace(/\s+/g, '-').toLowerCase();
-      
+        // const name = componentName.replace(/\s+/g, '-').toLowerCase();
         const svg = (await axios(componentUrl)).data;
-        const filePath = path.join(__dirname, 'src/svgs', `${name}.svg`);
+        const filePath = path.join(__dirname, 'src/svgs', `${componentName}.svg`);
         fs.writeFileSync(filePath, svg);
-        console.log(`Wrote ${name}.svg`);
+        console.log(`Wrote ${componentName}.svg`);
       }
   })()
 })

af286b7d4b897e090d57cc10a1b95bbeff755c99
Josh Cusick <josh.m.cusick@gmail.com>
2023-04-17 15:39:23 -0400

    Update figmaWebhook.js

--- figmaWebhook.js
+++ figmaWebhook.js
@@ -1,43 +1,55 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const ngrok = require('ngrok')
-const axios = require('axios')
-const crypto = require('crypto')
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ngrok = require("ngrok");
+const axios = require("axios");
+const crypto = require("crypto");
+require("dotenv").config();
 
-const PORT = 3000
-const app = express()
-const passcode = crypto.randomBytes(48).toString('hex')
+const PORT = 3000;
+const app = express();
+const passcode = crypto.randomBytes(48).toString("hex");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/', (request, response) => {
+app.post("/", (request, response) => {
   if (request.body.passcode === passcode) {
-    const { file_name, timestamp, triggered_by, version, description, handle } = request.body
+    const { file_name, timestamp, triggered_by, version, description, handle } =
+      request.body;
 
-    console.log(`📁 File name: ${file_name}\n⏰ Updated on: ${timestamp}.\n` + (triggered_by && triggered_by.handle ? `😎 Updated by: ${triggered_by.handle}.\n` : '') + `📝 Notes: ${description}\n\n===================================================\n`)
-    response.sendStatus(200)
+    // handles the initial push from Figma of blank files that come in
+    if (!file_name) {
+      return null;
+    } else {
+      console.log(
+        `📁 File name: ${file_name}\n⏰ Updated on: ${timestamp}.\n` +
+          (triggered_by && triggered_by.handle
+            ? `😎 Updated by: ${triggered_by.handle}.\n`
+            : "") +
+          `📝 Notes: ${description}\n\n===================================================\n`
+      );
+      response.sendStatus(200);
+    }
   } else {
-    response.sendStatus(403)
+    response.sendStatus(403);
   }
-})
+});
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 ngrok.connect(PORT).then(async (endpoint) => {
   const response = await axios({
-    url: 'https://api.figma.com/v2/webhooks',
-    method: 'post',
+    url: "https://api.figma.com/v2/webhooks",
+    method: "post",
     headers: {
-      'X-Figma-Token': process.env.FIGMA_API_KEY,
+      "X-Figma-Token": process.env.FIGMA_API_KEY,
     },
     data: {
       file_key: process.env.FIGMA_DOC_ID,
-      event_type: 'LIBRARY_PUBLISH',
+      event_type: "LIBRARY_PUBLISH",
       team_id: process.env.FIGMA_TEAM_ID,
       passcode,
       endpoint,
     },
-  })
-  console.log(`🎣 Webhook ${response.data.id} successfully created`)
-})
+  });
+  console.log(`🎣 Webhook ${response.data.id} successfully created\n`);
+});

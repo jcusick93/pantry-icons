3c5bf06724a12acbda7364bb03c8e91e219a046d
Josh Cusick <josh.m.cusick@gmail.com>
2023-04-24 17:57:44 -0400

    Updated the buildAndroid.js

--- buildAndroid.js
+++ buildAndroid.js
@@ -1,14 +1,19 @@
 const fs = require("fs");
 const { JSDOM } = require("jsdom");
-
 const svgsDir = "./dist/svgs";
 const androidDir = "./dist/android";
+
+console.log(`🤖 Building XML files for Android...\n`);
 
 fs.readdir(svgsDir, (err, files) => {
   if (err) {
     console.error(err);
     return;
   }
+
+  // Initializes the count of icons synced.
+  const totalIcons = files.length;
+  let totalIconsSynced = 0;
 
   files.forEach((file) => {
     const svg = fs.readFileSync(`${svgsDir}/${file}`, "utf8");
@@ -26,13 +31,11 @@ fs.readdir(svgsDir, (err, files) => {
 
     const path = document.querySelector("path").getAttribute("d");
 
-    const vectorXml = `<?xml version="1.0" encoding="utf-8"?>
-<vector xmlns:android="http://schemas.android.com/apk/res/android"
+    const vectorXml = `<vector xmlns:android="http://schemas.android.com/apk/res/android"
     android:width="${width}dp"
     android:height="${height}dp"
     android:viewportWidth="${viewBoxWidth}"
     android:viewportHeight="${viewBoxHeight}">
-
     <path
         android:fillColor="#000000"
         android:pathData="${path}" />
@@ -40,5 +43,11 @@ fs.readdir(svgsDir, (err, files) => {
 
     const fileName = file.replace(".svg", ".xml");
     fs.writeFileSync(`${androidDir}/${fileName}`, vectorXml);
+    console.log(`✅ Synced ${fileName}`);
+
+    totalIconsSynced++;
+    if (totalIconsSynced === totalIcons) {
+      console.log(`\n✨ Synced a total of ${totalIconsSynced} icons.\n`);
+    }
   });
 });
